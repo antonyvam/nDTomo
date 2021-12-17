df815ac150e8a05fb137da9feb5d0b5b7f4b4ab0
antonyvam <antonyvam@gmail.com>
2021-12-17 09:44:32 +0000

    Create losses_torch.py

---
+++ nDTomo/nn/losses_torch.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Antony
+
+"""
+#%%
+
+import torch
+import torch.nn as nn
+
+def dice_loss(pred, target, smooth = 1.):
+
+    '''
+    Dice loss taken from https://github.com/usuyama/pytorch-unet/blob/master/loss.py
+    '''
+    pred = pred.contiguous()
+    target = target.contiguous()    
+
+    intersection = (pred * target).sum(dim=2).sum(dim=2)
+    
+    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
+    
+    return loss.mean()
