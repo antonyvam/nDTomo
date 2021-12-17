01450e14a12e43d66d450ba09872fd20fca0adaa
antonyvam <antonyvam@gmail.com>
2021-12-17 15:35:34 +0000

    Update models_torch.py

--- nDTomo/nn/models_torch.py
+++ nDTomo/nn/models_torch.py
@@ -20,29 +20,42 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         
         self.conv2d_initial = nn.Sequential(nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
+                                    nn.Dropout2d(0.1),
                                     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
+                                    nn.Dropout2d(0.1),
                                     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
-                                    nn.BatchNorm2d(64))
+                                    nn.Dropout2d(0.1))
 
         self.conv2d_down = nn.Sequential(nn.Conv2d(64, 64, kernel_size=3, stride=2, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
+                                    nn.Dropout2d(0.1),
                                     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
+                                    nn.Dropout2d(0.1),
                                     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
-                                    nn.BatchNorm2d(64))
+                                    nn.Dropout2d(0.1))
 
         self.up = nn.Sequential(nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True),
                                 nn.Conv2d(64, 64, kernel_size=2, stride=1, padding=1, bias=False))
             
         self.conv2d_dual = nn.Sequential(nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
+                                    nn.Dropout2d(0.1),
                                     nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
                                     nn.ReLU(),
-                                    nn.BatchNorm2d(64))
+                                    nn.Dropout2d(0.1))
 
         self.out = nn.Conv2d(64, 1, kernel_size=1)
     
