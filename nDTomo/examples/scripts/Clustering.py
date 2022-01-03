7242daade73b2d58c745b8733631bbe00b81f045
antonyvam <antonyvam@gmail.com>
2022-01-03 18:28:20 +0200

    Update Clustering.py

--- nDTomo/examples/scripts/Clustering.py
+++ nDTomo/examples/scripts/Clustering.py
@@ -1,39 +1,92 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  4 14:14:52 2021
+Dimensionality reduction/ cluster analysis using a phantom xrd-ct dataset
 
-@author: Antony
+@author: Antony Vamvakeros
 """
-
-import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_digits
-from sklearn.decomposition import PCA, NMF, FastICA, LatentDirichletAllocation
-from sklearn.cluster import KMeans, SpectralClustering, DBSCAN, AgglomerativeClustering
-import h5py
-import hyperspy.api as hs
-
-from nDTomo.sim.shapes.phantoms import phantom5c_xanesct, phantom5c_xrdct
 
 #%%
 
+from nDTomo.sim.shapes.phantoms import phantom5c_xanesct, phantom5c_xrdct, load_example_patterns, phantom5c
+from nDTomo.utils import hyperexpl
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%% Create the chemical tomography dataset
+
+'''
+We will create a chemical tomography phantom using nDTomo
+Here we create an XRD-CT dataset using 5 chemical components; this corresponds to five unique spectra (diffraction patterns in this case) and five unique images
+This is a 3D matrix (array) with dimenion sizes (x, y, spectral): 200 x 200 x 250
+So this corresponds to 250 images, each having 200 x 200 pixels
+The goal is to perform dimensionality reduction/cluster analysis and extract these five images and/or spectra
+The various methods can be applied either to the image domain by treating the volume as a stack of images (250 images, each having 200 x 200 pixels), 
+or in the spectral domain (200 x 200 spectra with 250 points in each spectrum)
+'''
+
 npix = 200
 chemct = phantom5c_xrdct(npix)
+print(chemct.shape)
 
-hs.signals.Signal2D(np.transpose(chemct, (2,1,0))).plot()
- 
-#%% Load the dataset
 
-fn = 'data\Phantom_xrdct.h5'
-fn = 'data\Phantom_xanes.h5'
+#%% Visualise the hyperspectral volume
 
-with h5py.File(fn, 'r') as f:
-    xrdct = np.array(f['data'][:])
+hs = hyperexpl.HyperSliceExplorer(chemct, np.arange(0,chemct.shape[2]), 'Channels')
+hs.explore()
 
-xrdct = xrdct + np.min(xrdct)
+
+#%% Ground truth
+
+'''
+These are the five ground truth componet spectra
+'''
+
+dpAl, dpCu, dpFe, dpPt, dpZn, tth, q = load_example_patterns()
+
+plt.figure(1);plt.clf()
+plt.plot(dpAl)
+plt.plot(dpCu + 0.1)
+plt.plot(dpFe + 0.2)
+plt.plot(dpPt + 0.3)
+plt.plot(dpZn + 0.4)
+plt.show()
+
+
+'''
+These are the five ground truth componet images
+'''
+
+imAl, imCu, imFe, imPt, imZn = phantom5c(npix)
+
+plt.figure(2);plt.clf()
+plt.imshow(imAl)
+plt.show()
+
+plt.figure(3);plt.clf()
+plt.imshow(imCu)
+plt.show()
+
+plt.figure(4);plt.clf()
+plt.imshow(imFe)
+plt.show()
+
+plt.figure(5);plt.clf()
+plt.imshow(imPt)
+plt.show()
+
+plt.figure(6);plt.clf()
+plt.imshow(imZn)
+plt.show()
+
+
+#%% We can try first using the scikit-learn library
+
+from sklearn.decomposition import PCA, NMF, FastICA, LatentDirichletAllocation
+from sklearn.cluster import KMeans, SpectralClustering, DBSCAN, AgglomerativeClustering
+
 #%% PCA: Spectra
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2]))
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2]))
 
 print(data.shape)
 
@@ -49,7 +102,7 @@ plt.show()
 
 #%% PCA: Images
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2])).transpose()
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2])).transpose()
 
 print(data.shape)
 
@@ -59,7 +112,7 @@ print(pca.components_.shape)
 
 ii = 1
 im = pca.components_[ii,:]
-im = np.reshape(im, (xrdct.shape[0],xrdct.shape[1]))
+im = np.reshape(im, (chemct.shape[0],chemct.shape[1]))
 
 plt.figure(1);plt.clf()
 plt.imshow(im)
@@ -70,7 +123,7 @@ plt.show()
 #%% K means: images
 
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2])).transpose()
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2])).transpose()
 
 print(data.shape)
 
@@ -80,13 +133,13 @@ kmeans = KMeans(init="k-means++", n_clusters=clusters, n_init=4, random_state=0)
 
 labels = kmeans.labels_[:]
 
-ims = np.zeros((xrdct.shape[0], xrdct.shape[1], clusters))
+ims = np.zeros((chemct.shape[0], chemct.shape[1], clusters))
 inds = []
 for ii in range(clusters):
 
     inds.append(np.where(kmeans.labels_==ii))
     
-    ims[:,:,ii] = np.mean(xrdct[:,:,np.squeeze(inds[ii])], axis = 2)
+    ims[:,:,ii] = np.mean(chemct[:,:,np.squeeze(inds[ii])], axis = 2)
 
 
 ii = 3
@@ -100,7 +153,7 @@ plt.show()
 #%% AgglomerativeClustering: images
 
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2])).transpose()
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2])).transpose()
 
 print(data.shape)
 
@@ -110,13 +163,13 @@ ac = AgglomerativeClustering(distance_threshold=75, linkage="complete", n_cluste
 
 labels = ac.labels_[:]
 
-ims = np.zeros((xrdct.shape[0], xrdct.shape[1], np.max(labels)))
+ims = np.zeros((chemct.shape[0], chemct.shape[1], np.max(labels)))
 inds = []
 for ii in range(np.max(labels)):
 
     inds.append(np.where(ac.labels_==ii))
     
-    tmp = xrdct[:,:,np.squeeze(inds[ii])]
+    tmp = chemct[:,:,np.squeeze(inds[ii])]
     
     if len(tmp.shape)>2:
     
@@ -143,13 +196,13 @@ spc = SpectralClustering(affinity="nearest_neighbors", n_clusters=5, eigen_solve
 
 labels = spc.labels_[:]
 
-ims = np.zeros((xrdct.shape[0], xrdct.shape[1], clusters))
+ims = np.zeros((chemct.shape[0], chemct.shape[1], clusters))
 inds = []
 for ii in range(clusters):
 
     inds.append(np.where(spc.labels_==ii))
     
-    ims[:,:,ii] = np.mean(xrdct[:,:,np.squeeze(inds[ii])], axis = 2)
+    ims[:,:,ii] = np.mean(chemct[:,:,np.squeeze(inds[ii])], axis = 2)
 
 #%
 ii = 2
@@ -162,11 +215,9 @@ plt.show()
 
 
 
-
-
 #%% NMF: Images
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2])).transpose()
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2])).transpose()
 
 print(data.shape)
 
@@ -174,9 +225,9 @@ nmf = NMF(n_components=10).fit(data+0.01)
 print(nmf.components_.shape)
 
 #%%
-ii = 6
+ii = 1
 im = nmf.components_[ii,:]
-im = np.reshape(im, (xrdct.shape[0],xrdct.shape[1]))
+im = np.reshape(im, (chemct.shape[0],chemct.shape[1]))
 
 plt.figure(1);plt.clf()
 plt.imshow(im)
@@ -186,7 +237,7 @@ plt.show()
 
 #%% FastICA: Images
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2])).transpose()
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2])).transpose()
 
 print(data.shape)
 
@@ -196,7 +247,7 @@ print(nmf.components_.shape)
 #%%
 ii = 2
 im = fica.components_[ii,:]
-im = np.reshape(im, (xrdct.shape[0],xrdct.shape[1]))
+im = np.reshape(im, (chemct.shape[0],chemct.shape[1]))
 
 plt.figure(1);plt.clf()
 plt.imshow(im)
@@ -205,11 +256,9 @@ plt.show()
 
 
 
-
-
 #%% LatentDirichletAllocation: Spectra
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2]))
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2]))
 
 print(data.shape)
 
@@ -221,14 +270,14 @@ print(lda.components_.shape)
 plt.figure(2);plt.clf()
 for ii in range(5):
     
-    plt.plot(q, lda.components_[ii,:]/np.max(lda.components_[ii,:]) + 0.05*ii)
+    plt.plot(lda.components_[ii,:]/np.max(lda.components_[ii,:]) + 0.05*ii)
 
 plt.show()
 
 
 #%% FastICA: Spectra
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2]))
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2]))
 
 print(data.shape)
 
@@ -240,13 +289,13 @@ print(fica.components_.shape)
 plt.figure(2);plt.clf()
 for ii in range(5):
     
-    plt.plot(q, fica.components_[ii,:]/np.max(fica.components_[ii,:]) + 0.05*ii)
+    plt.plot(fica.components_[ii,:]/np.max(fica.components_[ii,:]) + 0.05*ii)
 
 plt.show()
 
 #%% NMF: Spectra
 
-data = np.reshape(xrdct, (xrdct.shape[0]*xrdct.shape[1],xrdct.shape[2]))
+data = np.reshape(chemct, (chemct.shape[0]*chemct.shape[1],chemct.shape[2]))
 
 print(data.shape)
 
@@ -258,37 +307,6 @@ print(nmf.components_.shape)
 plt.figure(2);plt.clf()
 for ii in range(5):
     
-    plt.plot(E, nmf.components_[ii,:]/np.max(nmf.components_[ii,:]) + 0.05*ii)
+    plt.plot( nmf.components_[ii,:]/np.max(nmf.components_[ii,:]) + 0.05*ii)
 
 plt.show()
-
-#%%
-
-plt.figure(1);plt.clf()
-
-plt.plot(q, dpAl)
-plt.plot(q, dpPt+0.05*1)
-plt.plot(q, dpCu+0.05*2)
-plt.plot(q, dpFe+0.05*3)
-plt.plot(q, dpZn+0.05*4)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
