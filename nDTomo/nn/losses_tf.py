27df64377ee408d61206058da0c15be91353ea32
antonyvam <antonyvam@gmail.com>
2022-02-24 17:55:24 +0000

    Update losses_tf.py

--- nDTomo/nn/losses_tf.py
+++ nDTomo/nn/losses_tf.py
@@ -33,3 +33,65 @@ def dice_coef(y_true, y_pred, smooth=1):
 
 def dice_coef_loss(y_true, y_pred):
     return 1-dice_coef(y_true, y_pred)
+
+def penalty(img):
+
+    image = img[:, :, 0]
+    mean = tf.zeros(image.shape, dtype=tf.dtypes.float32, name=None)
+    ind0 = image.shape[0]
+    ind1 = image.shape[1]
+    image_pad1 = tf.pad(image, tf.constant([[1, 1,], [1, 1]]), "REFLECT")
+    image_pad2 = tf.pad(image, tf.constant([[2, 2,], [2, 2]]), "REFLECT")
+    mean_list = []
+    for i in range(3):
+        for j in range(3):
+            # if i != 1 and j != 1:
+            mean += image_pad1[i:i+ind0, j:j+ind1]
+            mean_list.append(image_pad1[i:i+ind0, j:j+ind1])
+    mean_list = tf.stack(mean_list)
+
+    mean = mean/9
+    mean_pad = tf.pad(mean, tf.constant([[1, 1], [1, 1]]), "REFLECT")
+    contribution = tf.zeros(image.shape, dtype=tf.dtypes.float32, name=None)
+
+    individual = []
+    for i in range(3):
+        for j in range(3):
+            # if i != 1 and j != 1:
+            individual.append(mean_pad[i:i+ind0, j:j+ind1])
+    individual = tf.stack(individual)
+    individual_min = tf.math.reduce_min(individual, axis=0, keepdims=False, name=None)
+
+    individual = individual-individual_min
+    contribution = tf.reduce_sum(individual, axis = 0)
+    contribution = individual / contribution
+
+    #normalize and take the inverse
+    contribution = contribution - contribution[4]
+    contribution = tf.abs(contribution)
+    contribution = tf.clip_by_value(contribution, 0.001, 1)
+    index_list = [0,1,2,3,5,6,7,8]
+    contribution = 1/ (tf.gather(contribution, index_list))
+
+    #normalize again
+    contribution_min = tf.math.reduce_min(contribution, axis=0, keepdims=False, name=None)
+    contribution = contribution - contribution_min
+    contribution_sum = tf.reduce_sum(contribution, axis = 0)
+    contribution = contribution / contribution_sum
+ 
+    mean_list = tf.gather(mean_list, index_list)
+    mean_contrib = tf.reduce_sum(mean_list * contribution, axis = 0)
+    # mean_contrib += mean
+    # mean_contrib /= 2
+    # for i in range(5):
+    #     for j in range(5):
+    #         mean += image_pad2[i:i+ind0, j:j+ind1]/5
+    # mean = mean/15
+    # mean =  0
+    # for i in range(image.shape[0]):
+    #     for j in range(image.shape[1]):
+    #         mean += tf.math.reduce_mean(image[i-1:i+2, j-1:j+2]) - image[i,j]
+
+    # return mean_contrib
+    # return mean_contrib
+    return tf.reduce_mean(tf.abs(mean_contrib - image)**2)
