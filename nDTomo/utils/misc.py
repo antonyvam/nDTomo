072f429145fb5865926540db33a20aa033ef8bce
antonyvam <antonyvam@gmail.com>
2022-03-02 17:18:43 +0000

    Update misc.py

--- nDTomo/utils/misc.py
+++ nDTomo/utils/misc.py
@@ -585,6 +585,7 @@ def cart2pol(x, y):
     
     rho = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y, x)
+    
     return(rho, phi)
 
 def pol2cart(rho, phi):
@@ -595,6 +596,7 @@ def pol2cart(rho, phi):
 
     x = rho * np.cos(phi)
     y = rho * np.sin(phi)
+    
     return(x, y)
 
 
