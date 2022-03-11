e3d1e7a4994c7d5fe6dac985c94c6ba42431bae5
antonyvam <antonyvam@gmail.com>
2022-03-11 09:51:47 +0000

    Update models_tf.py

--- nDTomo/nn/models_tf.py
+++ nDTomo/nn/models_tf.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Conv1D, UpSampling1D, Activation, Subtract, LeakyReLU, LayerNormalization, SpatialDropout2D, Average, Add, Input, concatenate, UpSampling2D, Reshape, Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization, Cropping2D
 
-
+	
 def DnCNN(npix, nlayers = 15, skip='Yes', filts = 64):
     
     '''
@@ -552,3 +552,69 @@ def Dense2D(npix, nlayers = 4, nodes = [100, 75, 50, 25], dropout = 'No', batchn
     model = Model(image_in, dlayer)
 
     return model
+	
+	
+
+def edsr(scale, num_filters=64, num_res_blocks=8, res_block_scaling=None):
+
+	'''
+	EDSR model taken from https://github.com/krasserm/super-resolution/
+	'''
+	
+    x_in = Input(shape=(None, None, 3))
+    x = Lambda(normalize)(x_in)
+
+    x = b = Conv2D(num_filters, 3, padding='same')(x)
+    for i in range(num_res_blocks):
+        b = res_block(b, num_filters, res_block_scaling)
+    b = Conv2D(num_filters, 3, padding='same')(b)
+    x = Add()([x, b])
+
+    x = upsample(x, scale, num_filters)
+    x = Conv2D(3, 3, padding='same')(x)
+
+    x = Lambda(denormalize)(x)
+    return Model(x_in, x, name="edsr")
+
+
+def res_block(x_in, filters, scaling):
+
+	'''
+	Residual block taken from https://github.com/krasserm/super-resolution/
+	'''
+	
+    x = Conv2D(filters, 3, padding='same', activation='relu')(x_in)
+    x = Conv2D(filters, 3, padding='same')(x)
+    if scaling:
+        x = Lambda(lambda t: t * scaling)(x)
+    x = Add()([x_in, x])
+    return x
+
+
+def upsample(x, scale, num_filters):
+
+	'''
+	Upsample layer taken from https://github.com/krasserm/super-resolution/
+	'''
+	
+    def upsample_1(x, factor, **kwargs):
+        x = Conv2D(num_filters * (factor ** 2), 3, padding='same', **kwargs)(x)
+        return Lambda(pixel_shuffle(scale=factor))(x)
+
+    if scale == 2:
+        x = upsample_1(x, 2, name='conv2d_1_scale_2')
+    elif scale == 3:
+        x = upsample_1(x, 3, name='conv2d_1_scale_3')
+    elif scale == 4:
+        x = upsample_1(x, 2, name='conv2d_1_scale_2')
+        x = upsample_1(x, 2, name='conv2d_2_scale_2')
+
+    return x
+	
+def pixel_shuffle(scale):
+
+	'''
+	Pixel shuffle function taken from https://github.com/krasserm/super-resolution/
+	'''
+	
+    return lambda x: tf.nn.depth_to_space(x, scale)
