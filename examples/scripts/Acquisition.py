d9c0d2b53cff1b9f89827ba7359532b82d2d448c
antonyvam <antonyvam@gmail.com>
2022-04-28 15:39:53 +0100

    Update Acquisition.py

--- examples/scripts/Acquisition.py
+++ examples/scripts/Acquisition.py
@@ -26,16 +26,25 @@ Acq.savedir = savedir
 Acq.dname = 'xrdct_test'
 
 #%%
+p = ndtomopath()
+poni = "%sexamples\\xrd2D\\CeO2.poni" %p
+savedir = 'Y:\\Antony\\nDTomo\\test2'
+
 
 Acq = nDVAcq(file_format ='cbf', scantype = 'Zigzig', fastaxis = 'Translation', slowaxis = 'Rotation', units = "q_A^-1")
 
-Acq.setwvl(wvl=0.)
-Acq.setxrdprms(dist=0.5, poni1=0, poni2=0, rot1=0, rot2=0, rot3=0)
+Acq.setwvl(wvl=1.5e-11)
+Acq.setxrdprms(dist=0.35, poni1=(500/2)*0.000172, poni2=(500/2)*0.000172, rot1=0, rot2=0, rot3=0)
+Acq.setdetector(shape=(500,500))
+Acq.azimint()
+Acq.create_nDTomo_phantom(npix = 100, nproj = 110)
+Acq.savedir = savedir
+Acq.dname = 'xrdct_test'
 
 #%%
 
 im = Acq.conv1Dto2D( Acq.data[50,50,:])
-im = addpnoise2D(im, ct = 0.01)
+im = addpnoise2D(im, ct = 0.05)
 
 showim(im,1,cmap='gray')
 
@@ -49,7 +58,7 @@ Acq.readscanprms()
 
 #%% Perform the XRD-CT scan
 
-Acq.start_scan(addnoise = 'Yes', ct = 0.01)
+Acq.start_scan(addnoise = 'Yes', ct = 0.05)
 
 
 
