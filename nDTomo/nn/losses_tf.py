bc1e8cfa1061a1b06fff7c83e11ae1bd5aab347f
antonyvam <antonyvam@gmail.com>
2022-07-27 14:14:46 +0100

    Update losses_tf.py

--- nDTomo/nn/losses_tf.py
+++ nDTomo/nn/losses_tf.py
@@ -33,3 +33,9 @@ def dice_coef(y_true, y_pred, smooth=1):
 
 def dice_coef_loss(y_true, y_pred):
     return 1-dice_coef(y_true, y_pred)
+
+def discriminator_loss(loss_object, real_output, fake_output):
+    real_loss = loss_object(tf.ones_like(real_output), real_output)
+    fake_loss = loss_object(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
