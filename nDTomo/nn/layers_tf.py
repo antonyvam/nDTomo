132b4803e96ded203bbc3fceef9d3f899370598b
antonyvam <antonyvam@gmail.com>
2023-01-12 14:11:57 +0000

    Update layers_tf.py

--- nDTomo/nn/layers_tf.py
+++ nDTomo/nn/layers_tf.py
@@ -6,8 +6,8 @@ Custom layers for Tensorflow
 """
 
 from tensorflow.keras import layers
-from tensorflow import matmul
-from tensorflow.nn import softmax
+from tensorflow import matmul, multiply, reduce_mean
+from tensorflow.nn import softmax, sigmoid
 
 class SelfAttention(layers.Layer):
     def __init__(self, channels):
@@ -33,4 +33,31 @@ class SelfAttention(layers.Layer):
         attention = layers.Multiply()([inputs, attention])
         return attention
 
+class SpatialAttention2D(layers.Layer):
+    def __init__(self, **kwargs):
+        super(SpatialAttention2D, self).__init__(**kwargs)
+        self.conv1x1 = layers.Conv2D(filters=1, kernel_size=(1, 1), activation='relu')
     
+    def call(self, inputs):
+        # reduce dimensionality of input feature maps
+        attention_maps = self.conv1x1(inputs)
+        # apply softmax to create attention maps
+        attention_maps = softmax(attention_maps, axis=-1)
+        # apply attention maps to input feature maps
+        attention_output = multiply(inputs, attention_maps)
+        return attention_output
+
+class ScaleAwareAttention2D(layers.Layer):
+    def __init__(self, **kwargs):
+        super(ScaleAwareAttention2D, self).__init__(**kwargs)
+
+    def call(self, inputs):
+        # apply global average pooling to input feature maps
+        global_average_pooling = reduce_mean(inputs, axis=[1, 2])
+        # apply softmax to create attention weights
+        attention_weights = softmax(global_average_pooling, axis=-1)
+        # apply attention weights to input feature maps
+        attention_output = multiply(inputs, attention_weights)
+        return attention_output
+
+
