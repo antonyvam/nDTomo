fff067117f9eaf55bfb002ba80914ec5104df4e1
antonyvam <antonyvam@gmail.com>
2023-02-13 14:21:24 +0000

    Update losses_torch.py

--- nDTomo/nn/losses_torch.py
+++ nDTomo/nn/losses_torch.py
@@ -23,3 +23,10 @@ def dice_loss(pred, target, smooth = 1.):
     loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) + target.sum(dim=2).sum(dim=2) + smooth)))
     
     return loss.mean()
+	
+def total_variation_loss(img, weight):
+     bs_img, c_img, h_img, w_img = img.size()
+     tv_h = torch.pow(img[:,:,1:,:]-img[:,:,:-1,:], 2).sum()
+     tv_w = torch.pow(img[:,:,:,1:]-img[:,:,:,:-1], 2).sum()
+     return weight*(tv_h+tv_w)/(bs_img*c_img*h_img*w_img)
+	 
