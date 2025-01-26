a933ec0492d81ac94defcd8af30ea92ccbb4d836
antonyvam <antonyvam@gmail.com>
2025-01-26 12:16:06 +0000

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = u'A. Vamvakeros'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'2021.12'
+release = u'2019.01'
 
 
 # -- General configuration ---------------------------------------------------
