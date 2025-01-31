47d1e8995e63ec5f16a2aa8b20b44084fe3aaec6
antonyvam <antonyvam@gmail.com>
2025-01-31 16:36:10 +0000

    Update __init__.py

---
+++ nDTomo/analysis/__init__.py
@@ -0,0 +1,19 @@
+"""
+Analysis module for nDTomo.
+
+This package contains modules for processing tomography data, including:
+- `peaks`: methods related to peak models and peak fitting
+"""
+
+import importlib
+import sys
+
+# List of submodules
+__all__ = []
+modules = ["test"]
+
+# Import all functions dynamically
+for module in modules:
+    mod = importlib.import_module(f".{module}", package=__name__)
+    globals().update({name: obj for name, obj in vars(mod).items() if not name.startswith("_")})
+    __all__.extend([name for name in vars(mod) if not name.startswith("_")])
