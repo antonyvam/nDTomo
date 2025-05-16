75c15be414a8e7a30c44131298e72af70d3ac811
antonyvam <antonyvam@gmail.com>
2025-05-16 16:17:03 +0100

    gui

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
 		"pyqtgraph", "scikit-image", "scikit-learn", "xdesign", "tqdm", "ipykernel",
 		"pystackreg", "tifffile", "jupyterlab", 
 		"algotom", "fabio","pyFAI>=2025.01", "napari",
-		"configobj",
+		"configobj", "ipympl",
 	],
     extras_require={
         "torch": ["torch", "torchvision", "torchaudio"]
